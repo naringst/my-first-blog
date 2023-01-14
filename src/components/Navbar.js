866499185f634fb002b3fc6c01c18ccfe88a39ba
Ellie <naringst@gmail.com>
2023-01-14 23:45:32 +0900

    Add a navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Home</div>
+      <div>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+        <p>Blog</p>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
