f1b58d7f1b2be00fdfd538ff74e7419fa2c58d82
Ellie <naringst@gmail.com>
2023-01-19 20:34:16 +0900

    style : add google fonts on project

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,31 @@
 module.exports = {
   siteMetadata: {
     title: `My first blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Lato`,
+          `Nunito Sans`,
+          `Montserrat:500,600,700,800,900`,
+          `Poiret One\:400`,
+          `Yeseva One\:400`,
+        ],
+        display: "swap",
+      },
+    },
+  ],
 };
